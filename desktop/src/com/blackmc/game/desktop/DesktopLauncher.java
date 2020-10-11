4144fc772bff5df7afd8f53b180b11432b205c0c
Piotr Suchecki <piotrek.suchecki@vp.pl>
2020-10-11 13:47:14 +0200

    Initial commit

---
+++ desktop/src/com/blackmc/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blackmc.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blackmc.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
